9283ebdf0ee215aaf97770bdd5d898531af94d93
Nagarjun Vinukonda <49041896+nagarjunvinukonda@users.noreply.github.com>
2023-08-03 21:02:36 -0400

    Update subs.cpp
    spell change fanthom to fathom and remove unneeded code

--- src/subs.cpp
+++ src/subs.cpp
@@ -1,72 +1,6 @@
-// #include <ros/ros.h>
-// #include <fanthom_radiant/DiffData.h>
-// #include <fanthom_radiant/CustomData.h>
-// #include <std_msgs/Float32.h>
-// #include <boost/date_time.hpp>
-
-// ros::Time last_time;
-// ros::Publisher diff_pub;  // Declaration for diff_pub
-
-// // void custom_data_callback(const fanthom_radiant::CustomData::ConstPtr& data)
-// // {
-// //     const boost::posix_time::ptime current_time = boost::posix_time::from_iso_extended_string(data->time);
-
-// //     if (last_time != ros::Time(0))
-// //     {
-// //         const double time_diff = (current_time - boost::posix_time::from_time_t(last_time.toSec())).total_seconds();
-// //         fanthom_radiant::DiffData diff_msg;
-// //         diff_msg.time_difference = time_diff;
-// //         diff_pub.publish(diff_msg);
-// //     }
-
-// //     last_time = ros::Time::fromBoost(current_time);
-// // }
-
-
-// void custom_data_callback(const fanthom_radiant::CustomData::ConstPtr& data)
-// {
-//     if (data->time.empty())
-//     {
-//         ROS_WARN("Received CustomData message with empty time field.");
-//         return;
-//     }
-
-//     try
-//     {
-//         const boost::posix_time::ptime current_time = boost::posix_time::from_iso_extended_string(data->time);
-
-//         if (last_time != ros::Time(0))
-//         {
-//             const double time_diff = (current_time - boost::posix_time::from_time_t(last_time.toSec())).total_seconds();
-//             fanthom_radiant::DiffData diff_msg;
-//             diff_msg.time_difference = time_diff;
-//             diff_pub.publish(diff_msg);
-//         }
-
-//         last_time = ros::Time::fromBoost(current_time);
-//     }
-//     catch (const boost::bad_lexical_cast& e)
-//     {
-//         ROS_ERROR_STREAM("Error parsing time: " << e.what());
-//     }
-// }
-
-
-// int main(int argc, char** argv)
-// {
-//     ros::init(argc, argv, "csv_subscriber");
-//     ros::NodeHandle nh;
-
-//     diff_pub = nh.advertise<fanthom_radiant::DiffData>("/diff", 10);  // Initialize diff_pub
-//     // ros::Subscriber sub = nh.subscribe("data", 10, custom_data_callback);
-//     ros::Subscriber sub = nh.subscribe<fanthom_radiant::CustomData>("data", 10, custom_data_callback);
-//     ros::spin();
-    
-//     return 0;
-// }
 #include <ros/ros.h>
-#include <fanthom_radiant/CustomData.h>
-#include <fanthom_radiant/DiffData.h>
+#include <fathom_radiant/CustomData.h>
+#include <fathom_radiant/DiffData.h>
 #include <std_msgs/Float32.h>
 #include <chrono>
 #include <iomanip>
@@ -75,7 +9,7 @@
 ros::Publisher diff_pub;
 std::chrono::system_clock::time_point last_time;
 
-void customDataCallback(const fanthom_radiant::CustomData::ConstPtr& data)
+void customDataCallback(const fathom_radiant::CustomData::ConstPtr& data)
 {
     std::stringstream ss;
     ss << data->time; // Convert the string to a stringstream
@@ -87,7 +21,7 @@ void customDataCallback(const fanthom_radiant::CustomData::ConstPtr& data)
     if (last_time.time_since_epoch().count() != 0)
     {
         std::chrono::duration<double> time_diff = current_time - last_time;
-        fanthom_radiant::DiffData diff_msg;
+        fathom_radiant::DiffData diff_msg;
         diff_msg.time_difference = time_diff.count();
         diff_pub.publish(diff_msg);
     }
@@ -101,7 +35,7 @@ int main(int argc, char** argv)
     ros::NodeHandle nh;
 
     ros::Subscriber custom_data_sub = nh.subscribe("data", 10, customDataCallback);
-    diff_pub = nh.advertise<fanthom_radiant::DiffData>("/diff", 10);
+    diff_pub = nh.advertise<fathom_radiant::DiffData>("/diff", 10);
 
     ros::spin();
 
